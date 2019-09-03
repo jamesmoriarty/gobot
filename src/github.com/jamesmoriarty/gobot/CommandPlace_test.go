a539add2000fed9aec7adca45407fda2f12efa07
James Moriarty <jamespaulmoriarty@gmail.com>
2019-09-03 22:17:53 +1000

    refinement: increase code coverage and flag on in bin.

--- src/github.com/jamesmoriarty/gobot/CommandPlace_test.go
+++ src/github.com/jamesmoriarty/gobot/CommandPlace_test.go
@@ -2,6 +2,15 @@ package main
 
 import "testing"
 
+func TestCommandPlace(t *testing.T) {
+	tokens := []string{"place", "1", "2", "West"}
+	got, err := CommandPlace(tokens)
+
+	if err != nil {
+		t.Errorf("*%v != nil", got)
+	}
+}
+
 func TestCommandPlaceXError(t *testing.T) {
 	tokens := []string{"place", "zoo", "2", "West"}
 	got, err := CommandPlace(tokens)
@@ -11,6 +20,32 @@ func TestCommandPlaceXError(t *testing.T) {
 	}
 
 	if err.Error() != "invalid x" {
+		t.Errorf("*%v != err", err)
+	}
+}
+
+func TestCommandPlaceYError(t *testing.T) {
+	tokens := []string{"place", "1", "y", "West"}
+	got, err := CommandPlace(tokens)
+
+	if got != nil {
+		t.Errorf("*%v != nil", got)
+	}
+
+	if err.Error() != "invalid y" {
+		t.Errorf("*%v != err", err)
+	}
+}
+
+func TestCommandPlaceDirectionError(t *testing.T) {
+	tokens := []string{"place", "1", "1", "zoo"}
+	got, err := CommandPlace(tokens)
+
+	if got != nil {
+		t.Errorf("*%v != nil", got)
+	}
+
+	if err.Error() != "invalid direction" {
 		t.Errorf("*%v != err", err)
 	}
 }
