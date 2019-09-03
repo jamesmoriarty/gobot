385a2de45bf6bf8f798d3bc364e1a1eeeec070f0
James Moriarty <jamespaulmoriarty@gmail.com>
2019-09-03 19:22:25 +1000

    bugfix: tests and bin.

--- src/github.com/jamesmoriarty/gobot/Direction_test.go
+++ src/github.com/jamesmoriarty/gobot/Direction_test.go
@@ -1,13 +1,37 @@
 package main
 
 import (
-	"math"
 	"testing"
 )
 
-func TestAbs(t *testing.T) {
-	got := math.Abs(-1)
-	if got != 1 {
-		t.Errorf("Abs(-1) = %f; want 1", got)
+func TestNorthStringToDirection(t *testing.T) {
+	got, _ := StringToDirection("North")
+
+	if *got != North {
+		t.Errorf("*%v != North", got)
+	}
+}
+
+func TestEastStringToDirection(t *testing.T) {
+	got, _ := StringToDirection("East")
+
+	if *got != East {
+		t.Errorf("*%v != East", got)
+	}
+}
+
+func TestSouthStringToDirection(t *testing.T) {
+	got, _ := StringToDirection("South")
+
+	if *got != South {
+		t.Errorf("*%v != South", got)
+	}
+}
+
+func TestWestStringToDirection(t *testing.T) {
+	got, _ := StringToDirection("West")
+
+	if *got != West {
+		t.Errorf("*%v != West", got)
 	}
 }
